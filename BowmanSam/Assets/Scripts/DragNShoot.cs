c296c7a0c4ebcbc97ec06a1050f09941e8c08c8d
Slime Enjoyer <austinlanyon@gmail.com>
2023-07-20 09:54:22 +1000

    shit works now, now to turn it into an actual bow and arrow game

--- BowmanSam/Assets/Scripts/DragNShoot.cs
+++ BowmanSam/Assets/Scripts/DragNShoot.cs
@@ -44,8 +44,10 @@ public class DragNShoot : MonoBehaviour
             endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             endPoint.z = 15;
             Debug.Log(endPoint);
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
         }
-        force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
-        rb.AddForce(force * power, ForceMode2D.Impulse);
+
     }
 }
